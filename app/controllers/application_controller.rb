69efbd6373e09ba2d6cb7efa6267807f3d4e2f86
Chinedu Arinze <c.s.arinze@gmail.com>
2019-08-13 13:30:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,13 +13,14 @@ class App < Sinatra::Base
   end
 
   post '/teams' do
-    @team = Team.new(params[:team])
-
-    params[:team][:heros].each do |details|
-      Hero.new(details)
-    end
-    @heros = Hero.all
-    # @team_name = params[:team]
+    # @team = Team.new(params[:team])
+    #
+    # params[:team][:heros].each do |details|
+    #   Hero.new(details)
+    # end
+    # @heros = Hero.all
+    @team_name = params[:team][:name]
+    
     erb :team
   end
 end
